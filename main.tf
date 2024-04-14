88f3bea47425d278ae3fb46159f572e386ceafc8
Shashikant Patil <shashikantpatil2727@outlook.com>
2024-04-14 18:02:30 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=3.0.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
     azapi = {
       source = "azure/azapi"
@@ -22,56 +22,53 @@ resource "azurerm_resource_group" "my-rg" {
   location = "East US"  
 }
 
-# Creating log analytics workspace
 resource "azurerm_log_analytics_workspace" "law" {
-  name                = "law-aca-terraform" # Ensure the workspace name complies with the rules
+  name                = "law-aca-terraform"
   resource_group_name = azurerm_resource_group.my-rg.name
   location            = azurerm_resource_group.my-rg.location
 }
 
-# Creating aca environment
 resource "azapi_resource" "az_env" {
-  type       = "micrcosoft.app/managedenvironments@2022-03-01"
-  parent_id  = azurerm_resource_group.my-rg.id # Construct the parent ID correctly
+  type       = "microsoft.app/managedenvironments@2022-03-01"
+  parent_id  = azurerm_resource_group.my-rg.id
   location   = azurerm_resource_group.my-rg.location
   name       = "aca-env-terraform"
 
   body = jsonencode({
     properties = {
-      applogsconfiguration = {
+      appLogsConfiguration = { 
         destination = "log-analytics"
-        loganlyticsconfiguration = {
-          customerid = azurerm_log_analytics_workspace.law.workspace_id
-          sharedkey  = azurerm_log_analytics_workspace.law.primary_shared_key
+        logAnalyticsConfiguration = { 
+          customerId = azurerm_log_analytics_workspace.law.workspace_id
+          sharedKey  = azurerm_log_analytics_workspace.law.primary_shared_key
         }
       }
     }
   })
 }
 
-# Creating the aca (generic resource)
 resource "azapi_resource" "az_environment" {
-  type = "micrcosoft.app/containerapps@2022-03-01"
-  parent_id = azurerm_resource_group.my-rg.id # Construct the parent ID correctly
+  type = "microsoft.app/containerapps@2022-03-01"
+  parent_id = azurerm_resource_group.my-rg.id
   location = azurerm_resource_group.my-rg.location
   name = "terraform-app"
 
   body = jsonencode ({
     properties : {
-      managedenvironmentid = azapi_resource.az_env.id # Reference the correct resource
+      managedEnvironmentId = azapi_resource.az_env.id
       configuration = {
         ingress = {
           external   = true
-          targetport = 80
+          targetPort = 80
         }
       }
       template = {
         containers = [
           {
             name     = "web"
-            image    = "nginix" # Correct the image name to "nginx"
-            resource = {
-               CPU    = 0.5
+            image    = "nginx"
+            resources = {
+               cpu    = 0.5 # Adjusted property name to lowercase
                memory = "1.0Gi"
             }
           }        
@@ -84,3 +81,5 @@ resource "azapi_resource" "az_environment" {
     }
   })
 }
+
+
